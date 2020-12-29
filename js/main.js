55cc0ef71f202f9382ad0dc13ad8431b1d75b6f7
nelapudi sandeep <46179808+nelapudisandeep@users.noreply.github.com>
2020-12-29 17:17:54 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,44 @@
-const url = '../docs/pdf.pdf';
+let url = '../docs/pdf.pdf';
+const opener = document.querySelector("#opener");
+
+
+// opener click event
+let submit = document.querySelector("#submit");
+let form = document.querySelector(".form");
+
+submit.addEventListener("click",e=>{
+  e.preventDefault();
+  opener.style.display = "flex";
+  form.style.display = "none";
+  let formdata = new FormData(form);
+  let data = formdata.get("subject");
+  url = "../docs/" + data + ".pdf" ;
+  console.log(url);
+  // Get Document
+  
+  pdfjsLib
+    .getDocument(url)
+    .promise.then(pdfDoc_ => {
+      pdfDoc = pdfDoc_;
+
+      document.querySelector('#page-count').textContent = pdfDoc.numPages;
+
+      renderPage(pageNum);
+    })
+    .catch(err => {
+      // Display error
+      const div = document.createElement('div');
+      div.className = 'error';
+      div.appendChild(document.createTextNode(err.message));
+      document.querySelector('body').insertBefore(div, canvas);
+      // Remove top bar
+      document.querySelector('.top-bar').style.display = 'none';
+    });
+
+});
+
+
+
 
 let pdfDoc = null,
   pageNum = 1,
@@ -66,26 +106,77 @@ const showNextPage = () => {
   queueRenderPage(pageNum);
 };
 
-// Get Document
-pdfjsLib
-  .getDocument(url)
-  .promise.then(pdfDoc_ => {
-    pdfDoc = pdfDoc_;
-
-    document.querySelector('#page-count').textContent = pdfDoc.numPages;
-
-    renderPage(pageNum);
-  })
-  .catch(err => {
-    // Display error
-    const div = document.createElement('div');
-    div.className = 'error';
-    div.appendChild(document.createTextNode(err.message));
-    document.querySelector('body').insertBefore(div, canvas);
-    // Remove top bar
-    document.querySelector('.top-bar').style.display = 'none';
-  });
 
 // Button Events
 document.querySelector('#prev-page').addEventListener('click', showPrevPage);
 document.querySelector('#next-page').addEventListener('click', showNextPage);
+
+
+document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
+  const dropZoneElement = inputElement.closest(".drop-zone");
+
+  dropZoneElement.addEventListener("click", (e) => {
+    inputElement.click();
+  });
+
+  inputElement.addEventListener("change", (e) => {
+    if (inputElement.files.length) {
+      updateThumbnail(dropZoneElement, inputElement.files[0]);
+    }
+  });
+
+  dropZoneElement.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    dropZoneElement.classList.add("drop-zone--over");
+  });
+
+  ["dragleave", "dragend"].forEach((type) => {
+    dropZoneElement.addEventListener(type, (e) => {
+      dropZoneElement.classList.remove("drop-zone--over");
+    });
+  });
+
+  dropZoneElement.addEventListener("drop", (e) => {
+    e.preventDefault();
+
+    if (e.dataTransfer.files.length) {
+      inputElement.files = e.dataTransfer.files;
+      updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+    }
+
+    dropZoneElement.classList.remove("drop-zone--over");
+  });
+});
+
+
+function updateThumbnail(dropZoneElement, file) {
+  let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
+
+  // First time - remove the prompt
+  if (dropZoneElement.querySelector(".drop-zone__prompt")) {
+    dropZoneElement.querySelector(".drop-zone__prompt").remove();
+  }
+
+  // First time - there is no thumbnail element, so lets create it
+  if (!thumbnailElement) {
+    thumbnailElement = document.createElement("div");
+    thumbnailElement.classList.add("drop-zone__thumb");
+    dropZoneElement.appendChild(thumbnailElement);
+  }
+
+  thumbnailElement.dataset.label = file.name;
+
+  // Show thumbnail for image files
+  if (file.type.startsWith("image/")) {
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
+    };
+  } else {
+    thumbnailElement.style.backgroundImage = null;
+  }
+}
+
+
